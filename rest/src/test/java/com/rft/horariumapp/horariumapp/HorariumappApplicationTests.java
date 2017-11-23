8adfaf6476e74e6c66159856bf6c571cfc51cf01
Nyiri Krisztian <ny.krisz04@gmail.com>
2017-11-23 22:51:31 +0100

    init

---
+++ rest/src/test/java/com/rft/horariumapp/horariumapp/HorariumappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rft.horariumapp.horariumapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HorariumappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
